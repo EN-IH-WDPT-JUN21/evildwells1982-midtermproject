7e53019c35563db29f4030b6a9795e903299e190
Lee Dawson <lee.dawson1982@hotmail.co.uk>
2021-09-16 23:48:32 +0100

    transaction entity constructors done

--- src/main/java/com/ironhack/midtermproject/dao/Transactions.java
+++ src/main/java/com/ironhack/midtermproject/dao/Transactions.java
@@ -1,9 +1,18 @@
 package com.ironhack.midtermproject.dao;
 
+import com.ironhack.midtermproject.enums.TransactionTypes;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
-
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Transactions {
     @Id
@@ -23,10 +32,50 @@ public class Transactions {
 
     private final LocalDateTime transactionDateTime = LocalDateTime.now();
 
+    @Enumerated
+    private TransactionTypes transactionType;
+
     @ManyToOne
     @JoinColumn(name = "performedBy")
     private User performedBy;
 
 
+    //Constructor for Customers Paying Customers, and admins transferring between accounts
+    public Transactions(Account sourceAccount, Account destinationAccount, Money transactionAmount, User performedBy) {
+        setSourceAccount(sourceAccount);
+        setDestinationAccount(destinationAccount);
+        setTransactionAmount(transactionAmount);
+        setTransactionType(TransactionTypes.ACCOUNTTOACCOUNT);;
+        setPerformedBy(performedBy);
+    }
 
+    //Constructor for paying out to third parties, and for Admins removing money from accounts
+    public Transactions(Account sourceAccount, Money transactionAmount, TransactionTypes transactionType, User performedBy) {
+        setSourceAccount(sourceAccount);
+        setTransactionAmount(transactionAmount);
+        setTransactionType(transactionType);
+        setPerformedBy(performedBy);
+    }
+
+    //Constructor for money in from third parties, and for Admins adding money to accounts
+    public Transactions(Money transactionAmount, Account destinationAccount, TransactionTypes transactionType, User performedBy) {
+        setDestinationAccount(destinationAccount);
+        setTransactionAmount(transactionAmount);
+        setTransactionType(transactionType);
+        setPerformedBy(performedBy);
+    }
+
+    //Constructor for Interest adding to account
+    public Transactions(Account destinationAccount, Money transactionAmount) {
+        setDestinationAccount(destinationAccount);
+        setTransactionAmount(transactionAmount);
+        setTransactionType(TransactionTypes.INTEREST);
+    }
+
+    //constructor for interest, fees etc going from account
+    public Transactions(Account sourceAccount, Money transactionAmount, TransactionTypes transactionType) {
+        setSourceAccount(sourceAccount);
+        setTransactionAmount(transactionAmount);
+        setTransactionType(transactionType);
+    }
 }
